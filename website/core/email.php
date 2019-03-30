b4f0b75704ae948d699420a63866560e910eeb90
Darius Atyeh <46077662+DariusNorwood@users.noreply.github.com>
2019-03-30 16:49:20 -0400

    Create email.php

---
+++ website/core/email.php
@@ -0,0 +1,13 @@
+<?php
+$GLOBALS['email'] = array(
+  'email' => '',
+  'username' => '',
+  'password' => '',
+  'name' => '',
+  'host' => '',
+  'port' => 587,
+  'secure' => 'tls',
+  'smtp_auth' => true,
+);
+
+*/This is unused
